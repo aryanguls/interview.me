8723c4d6610a897f93782a16e685493ad73c7830
Aryan Gulati <aryanguls@gmail.com>
2024-09-07 18:30:42 +0530

    updated gitignore and scripts

---
+++ scripts/resume_to_text.py
@@ -0,0 +1,24 @@
+import docx
+import PyPDF2
+
+def extract_text_from_pdf(file):
+    pdf_reader = PyPDF2.PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+def extract_text_from_docx(file):
+    doc = docx.Document(file)
+    text = ""
+    for para in doc.paragraphs:
+        text += para.text + "\n"
+    return text
+
+if __name__ == "__main__":
+    file_path = "Aryan_Gulati_Resume.pdf"
+    if file_path.endswith(".pdf"):
+        text = extract_text_from_pdf(file_path)
+    elif file_path.endswith(".docx"):
+        text = extract_text_from_docx(file_path)
+    print(text)
